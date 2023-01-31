a453dcc40dc07ebf9f0c365175ffcd9120b095e4
freshwaterhealth <96105572+freshwaterhealth@users.noreply.github.com>
2023-01-31 09:58:09 +0800

    gwwt version2

---
+++ gswwt/Home.py
@@ -0,0 +1,235 @@
+import streamlit as st
+import xml.etree.ElementTree as ET
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import urllib.request as url
+
+st.set_page_config(
+    layout="wide", 
+    page_title="FHI Survey",
+    menu_items={
+        'Get Help':'https://www.freshwaterhealthindex.org/',
+        'About':'https://www.freshwaterhealthindex.org/',
+        'Report a bug':'https://www.freshwaterhealthindex.org/'
+})
+
+#Map of XML entry to session state fhi_conc, 1->Ecosystem Services, etc
+conMapRev=[1,7,8,9,2,10,11,12,13]
+
+url_blank='https://webtools.freshwaterhealthindex.org/asset/sample.xml'
+#geomteric mean
+def gm_df(x,y):
+    a=[]
+    b=[]
+    for i in range(y):
+        a.append(float(st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'score']))
+        b.append(float(st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'weight']))
+    c=list(map(pow,a,b))   
+    return round(np.prod(c),2)
+
+# Initialization
+def xml2df(s): 
+    tree = ET.parse(s)
+    root = tree.getroot()
+    indNames=[]
+    indID=[]
+    indScores=[]
+    indWeights=[]
+    indDescp=[]
+    indCon=[]
+    for ind in root:
+        indID.append(ind.attrib["id"])    
+        for inm in ind.findall('name'):
+            indNames.append(inm.text)
+        for isc in ind.findall('score'):
+            indScores.append(isc.text)
+        for iwt in ind.findall('weight'):
+            indWeights.append(iwt.text)
+        for ide in ind.findall('description'):
+            indDescp.append(ide.text)
+    for x in conMapRev:
+        indCon.append(root[x].attrib["con"])   
+
+    # initialize data of lists.
+    data = {'name': indNames,
+            'id': indID,
+            'score':indScores,
+            'weight':indWeights,
+            'description':indDescp
+            }
+    # Create DataFrame
+    df = pd.DataFrame(data)
+    return root,df,indCon
+
+source = url.urlopen(url_blank)
+root,df,indCon=xml2df(source)
+#Session State Variables
+if 'fhi_df' not in st.session_state:
+    st.session_state.fhi_df = df
+if 'fhi_conc' not in st.session_state:
+    st.session_state.fhi_conc = indCon
+if 'fhi_wt' not in st.session_state:
+    st.session_state.fhi_wt = False
+if 'fhi_gs' not in st.session_state:
+    st.session_state.fhi_gs = False
+
+#Main display
+st.title("Freshwater Health Index Webtool")
+col1, col2 = st.columns(2)
+col2.markdown("![Alt Text](https://uploads-ssl.webflow.com/5d10fe385d27da1387c00f51/5f976713da23327149c6e327_ezgif.com-gif-maker%20(1).gif)")
+col1.subheader("Governance & Stakeholder Analysis")
+col1.markdown("Governance & Stakeholders is defined as \"the structures and processes by which people in societies make decisions and share power, creating the conditions for ordered rule and collective action, or institutions of social coordination\" (Schultz et al. 2015).")
+#col1.markdown("This definition encompasses multiple tiers of government, their formal rules and informal norms (e.g., community-established guidelines) and market mechanisms. It also encompasses a range of stakeholders comprising decision makers and the human beneficiary population (from individual citizens and community groups to municipalities, corporations and international organizations), as well as other stakeholders such as donor agencies, who may not directly benefit from the ecosystem services in a particular location, but nonetheless have an interest in, and influence over, decisions that affect a particular basin.")
+col1.markdown("For the Governance and Stakeholders component of the FHI, we have developed a survey instrument to measure this aspect by using four major indicators: Enabling Environment, Stakeholder Engagement, Vision and Adaptive Governance, and Effectiveness. Each of these indicators is composed by two up to five sub-indicators ([definitions](https://manual.freshwaterhealthindex.org/Governance_and_Stakeholders/Definitions.html)). This webtools helps a user analyse the outputs from the survey and add descriptions to each indicator score.")
+st.markdown("---")
+st.subheader("Instructions")
+st.markdown("1. To proceed with a new analysis, obtain the results of the weights and governance survey results for your basin from the FHI webtools, and upload the results in CSV format into the \"Add Weights\" and \"Add GS Survey\" pages. The summary results will be displayed below.")
+st.markdown("2. Alternatively, to visualize results from pre-existing analysis, upload the FHI Results XML file instead.")
+st.markdown("---")
+   
+uploaded_file = st.sidebar.file_uploader("Upload XML File",type=['xml'])
+if uploaded_file is not None:
+    try:
+        root,df,indCon=xml2df(uploaded_file)
+        df['weight']=df['weight'].astype(float)
+        df['score']=df['score'].astype(float)
+        st.session_state.fhi_df = df
+        st.session_state.fhi_conc = list(np.float_(indCon))
+        #Setting both true for now. later will need a better check
+        st.session_state.fhi_wt = True
+        st.session_state.fhi_gs = True
+    except:
+        st.error("Faliure loading results")
+        st.stop()
+
+if st.sidebar.button('Reset to blank'):
+    source = url.urlopen(url_blank)
+    root,df,indCon=xml2df(source)
+    st.session_state.fhi_df = df
+    st.session_state.fhi_conc = indCon
+    st.session_state.fhi_wt = False
+    st.session_state.fhi_gs = False
+
+if st.session_state.fhi_gs and st.session_state.fhi_wt:
+    df=st.session_state.fhi_df
+    df.at[df.index[10],'score']= gm_df(30,5)  
+    df.at[df.index[11],'score']= gm_df(35,2)
+    df.at[df.index[12],'score']= gm_df(37,2)
+    df.at[df.index[13],'score']= gm_df(39,3)
+
+    df.at[df.index[2],'score']= gm_df(10,4)
+
+    #Plots df    
+    df31=df.iloc[2:3,:]
+    df32=df.iloc[10:14,:]
+    df33=df.iloc[30:,:]
+    df3=pd.concat([df31,df32,df33])
+    df3=df3.reset_index(drop=True)
+    
+    pt=["",df3.at[df3.index[0],'name'],df3.at[df3.index[0],'name'],df3.at[df3.index[0],'name'],df3.at[df3.index[0],'name'],
+    df3.at[df3.index[1],'name'],df3.at[df3.index[1],'name'],df3.at[df3.index[1],'name'],df3.at[df3.index[1],'name'],df3.at[df3.index[1],'name'],
+    df3.at[df3.index[2],'name'],df3.at[df3.index[2],'name'],
+    df3.at[df3.index[3],'name'],df3.at[df3.index[3],'name'],
+    df3.at[df3.index[4],'name'],df3.at[df3.index[4],'name'],df3.at[df3.index[4],'name']
+    ]
+    ptw=[1,1,1,1,1,
+    df3.at[df3.index[1],'weight'],df3.at[df3.index[1],'weight'],df3.at[df3.index[1],'weight'],df3.at[df3.index[1],'weight'],df3.at[df3.index[1],'weight'],
+    df3.at[df3.index[2],'weight'],df3.at[df3.index[2],'weight'],
+    df3.at[df3.index[3],'weight'],df3.at[df3.index[3],'weight'],
+    df3.at[df3.index[4],'weight'],df3.at[df3.index[4],'weight'],df3.at[df3.index[4],'weight']
+    ]
+    s1 = pd.Series(pt, name="parent")
+    s2 = pd.Series(ptw, name="parentWeight")
+    df3 = pd.concat([df3, s1, s2], axis=1)
+    df3.at[df3.index[0],'weight']=1.0
+    df3['weight']=df3['weight'].astype(float)
+    df3['parentWeight']=df3['parentWeight'].astype(float)
+    df3['nweight']=df3.weight*df3['parentWeight']*100
+    df3['nweight']=df3['nweight'].round(2) 
+    df3['nweight']=df3['nweight'].astype(str)  
+
+    col1, col2 = st.columns(2)
+    cs = col2.selectbox(
+     'Select color palette for scores:',
+     ('blackbody', 'jet', 'rainbow','picnic','portland','spectral', 'curl','edge','hsv'))
+    rcs= col2.checkbox('Reverse color palette')
+    
+    fig = go.Figure()
+
+    fig.add_trace(go.Sunburst(
+            labels = df3.name,
+            ids = df3.name,
+            values = df3.nweight,            
+            parents = df3.parent,
+            marker=dict(
+                colors=df3.score,
+                colorscale=cs,
+                reversescale=rcs,
+                cmin=0, cmax=100,
+                colorbar=dict(thickness=20)), 
+            hovertemplate='<b>%{label} </b> <br> Score: %{color} </b> <br> Proportional Weight: %{value} </b>',
+            insidetextorientation='radial',
+            branchvalues="total",
+    ))
+    
+    fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+    # Plot! 
+    
+    col1.subheader("Overall Governance & Stakeholder score")
+    col1.metric(label="Score",
+                value= st.session_state.fhi_df.at[st.session_state.fhi_df.index[2],'score'])
+    txt=st.session_state.fhi_df.at[st.session_state.fhi_df.index[2],'description']
+    st.session_state.fhi_df.at[st.session_state.fhi_df.index[2],'description']=col1.text_area(
+        label="Add or update description", 
+        value=txt, 
+        key=st.session_state.fhi_df.at[st.session_state.fhi_df.index[2],'id'])
+    col2.plotly_chart(fig, use_container_width=False)
+
+    
+    #Add description    
+    x=10
+    for i in range(4):
+        st.subheader(st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'name'])
+        col1, col2 = st.columns([1,3])
+        col1.metric(label="Score",
+                    value= st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'score'])
+        txt=st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'description']
+        st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'description']=col2.text_area(
+            label="Add or update description", 
+            value=txt, 
+            key=st.session_state.fhi_df.at[st.session_state.fhi_df.index[x+i],'id'])
+    
+    df3=df3.drop(df3.index[0])
+    df3['score']=df3['score'].astype(float)
+    st.subheader("Results Summary")
+    cs = st.selectbox(
+     'Select color palette for weight:',
+     ('blackbody', 'jet', 'rainbow','picnic','portland','spectral', 'curl','edge','hsv'))
+    fig2 = px.bar(df3, x='name', y='score', color='weight', height=450, color_continuous_scale=cs)
+    st.plotly_chart(fig2, use_container_width=True)
+  
+
+#update root from df
+if st.session_state.fhi_gs or st.session_state.fhi_wt:
+    ii=0
+    df=st.session_state.fhi_df
+    #st.write(df)
+    for ind in root:
+        if(df.at[df.index[ii],'score'])!=None:
+            ind[1].text = str(df.at[df.index[ii],'score'])
+        if(df.at[df.index[ii],'weight'])!=None:
+            ind[2].text = str(df.at[df.index[ii],'weight'])
+        if(df.at[df.index[ii],'description'])!=None:
+            ind[3].text = df.at[df.index[ii],'description']
+        ii=ii+1
+    for x in range(9):
+        root[conMapRev[x]].attrib["con"]=str(st.session_state.fhi_conc[x])
+    upXML=ET.tostring(root)
+    st.download_button(
+                label="Download FHI Results XML file",
+                data=upXML,
+                file_name='fhi.xml',
+                mime='text/xml',
+                )
---
+++ gswwt/pages/1_Add Weights.py
@@ -0,0 +1,285 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(
+    layout="wide", 
+    page_title="FHI Weights",
+    menu_items={
+        'Get Help':'https://www.freshwaterhealthindex.org/',
+        'About':'https://www.freshwaterhealthindex.org/',
+        'Report a bug':'https://www.freshwaterhealthindex.org/'
+})
+
+st.title("FHI: Weight Survey Results")
+#Map of rows in session_state DF to dict ind row below, 7 -> Provisoning, etc
+rowMap=[7,8,9,22,23,24,25,26,27,28,29,10,11,12,13,30,31,32,33,34,35,36,37,38,39,40,41]
+#Map of order of con values to dict below
+conMap=[0,2,3,4,1,5,6,7,8]
+
+wtDict= {
+        "ES":{
+                "n":3,
+                "title": "Ecosystem Services",
+                "Indicator":["Provisioning","Regulation","Cultural"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "ES1":{
+                "n":2,
+                "title":"Provisioning",
+                "Indicator":["Water Supply Reliability","Biomass"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "ES2":{
+                "n":4,
+                "title":"Regulation",
+                "Indicator":["Sediment Regulation","Water Quality Regulation","Flood Regulation","Disease Regulation"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "ES3":{
+                "n":2,
+                "title":"Cultural",
+                "Indicator":["Conservation Areas","Recreation"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "GS":{
+                "n":4,
+                "title":"Governance & Stakeholders",
+                "Indicator":["Enabling Environment","Stakeholder Engagement","Vision and Adaptive Governance","Effectiveness"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "GS1":{
+                "n":5,
+                "title":"Enabling Environment",
+                "Indicator":["Water Resource Management","Rights to Resource Use","Incentives and Regulations","Financial Capacity","Technical Capacity"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "GS2":{
+                "n":2,
+                "title":"Stakeholder Engagement",
+                "Indicator":["Information Access","Engagement in Decision-Making"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "GS3":{
+                "n":2,
+                "title":"Vision and Adaptive Governance",
+                "Indicator":["Strategic Planning","Monitoring Mechanisms"],
+                "con":0.0,
+                "Weight":[]
+                },
+        "GS4":{
+                "n":3,
+                "title":"Effectiveness",
+                "Indicator":["Enforcement and Compliance","Distribution of Benefits","Water-Related Conflict"],
+                "con":0.0,
+                "Weight":[]
+                },
+        }
+weightFinal={
+        "indicators":[],
+        "weights":[]
+        }
+def dataFrameWeightClustering(df1):
+    df1.ES1=df1.ES1+df1.ES2+df1.ES3
+    df1.ES2=df1.ES1
+    df1.ES3=df1.ES1
+    df1.ES11=df1.ES11+df1.ES12
+    df1.ES12=df1.ES11
+    df1.ES21=df1.ES21+df1.ES22+df1.ES23+df1.ES24
+    df1.ES22=df1.ES21
+    df1.ES23=df1.ES21
+    df1.ES24=df1.ES21
+    df1.ES31=df1.ES31+df1.ES32
+    df1.ES32=df1.ES31
+    df1.GS1=df1.GS1+df1.GS2+df1.GS3+df1.GS4
+    df1.GS2=df1.GS1
+    df1.GS3=df1.GS1
+    df1.GS4=df1.GS1
+    df1.GS11=df1.GS11+df1.GS12+df1.GS13+df1.GS14+df1.GS15
+    df1.GS12=df1.GS11
+    df1.GS13=df1.GS11
+    df1.GS14=df1.GS11
+    df1.GS15=df1.GS11
+    df1.GS21=df1.GS21+df1.GS22
+    df1.GS22=df1.GS21
+    df1.GS31=df1.GS31+df1.GS32
+    df1.GS32=df1.GS31
+    df1.GS41=df1.GS41+df1.GS42+df1.GS43
+    df1.GS42=df1.GS41
+    df1.GS43=df1.GS41
+
+#Update dict from session_state df & list
+tempCon=st.session_state.fhi_conc
+dfTemp=st.session_state.fhi_df
+
+i=0
+ii=0
+if(tempCon[0])!="":
+    for x in wtDict:    
+        wtDict[x]["con"]=float(tempCon[conMap[ii]])
+        nn=wtDict[x]["n"]        
+        for j in range(nn):
+            wtDict[x]["Weight"].append(dfTemp.at[dfTemp.index[rowMap[i+j]],'weight'])
+        i=i+nn
+        ii=ii+1
+
+st.sidebar.title("Inputs")
+uploaded_file = st.sidebar.file_uploader("Upload CSV File",type=['csv'])
+if uploaded_file is not None:
+    try:
+        df=pd.read_csv(uploaded_file)
+        for x in wtDict:
+            wtDict[x]["Weight"].clear()
+            wtDict[x]["con"]=0.0
+    except:
+        st.error("Faliure loading results")
+        st.stop()
+    
+    sectorList=df.Sector.unique()
+    countryList=df.Country.unique()  
+    
+    if st.sidebar.checkbox('Subset by country'):
+        op1=st.sidebar.selectbox("Select country", countryList, index=0)
+        df=df.loc[df.Country == op1]
+
+
+    if st.sidebar.checkbox('Subset by sector'):
+        op2=st.sidebar.selectbox("Select sector", sectorList, index=0)
+        df=df.loc[df.Sector == op2]
+    
+    df.drop(['PID', 'Code', 'Country','Sector'], axis=1, inplace=True)
+    df=df.astype(float)
+    k=len(df.index)
+    
+    st.metric("Number of responses",k)
+    dfsum=df.copy()
+    dataFrameWeightClustering(dfsum)    
+    dfweight1=df.div(dfsum)
+        
+    #Weights!
+    dfweight=dfweight1.mean()
+    
+    def fln(x):
+        return -1*np.log(x)*x
+    
+    def fln2(x):
+        return np.exp(x)
+    
+    dfalpha=dfweight1.copy()
+    dfgamma=dfweight.copy()
+    
+    dfalpha=dfalpha.apply(fln)
+    dataFrameWeightClustering(dfalpha)
+
+    dfalphaD=dfalpha.mean()
+    dfgamma=dfgamma.apply(fln)
+    dataFrameWeightClustering(dfgamma)
+    dfBetaDiv=dfgamma-dfalphaD
+    dfBetaDiv=dfBetaDiv.apply(fln2)
+    #st.write(dfBetaDiv)
+    
+    def fnDalphaMin(n,m):
+        x1=1/(n+m-1.0)
+        x2=m/(n+m-1.0)
+        x3=-1*x2*np.log(x2)        
+        x5=(n-1)*x1*np.log(x1)
+        x6=x3-x5
+        x=np.exp(x6)
+        return x
+    
+    def fnDgammaMax(n,m,k):
+        x1=1/(n+m-1.0)
+        x2=(n-k)*(-1*x1*np.log(x1))
+        x3=k+m-1.0
+        x4=x3*x1*np.log(x3*x1/k)
+        x6=x2-x4
+        x=np.exp(x6)
+        return x
+    
+    def con(a,b,c):
+        x=b/c 
+        x1= 1.0-(b/c)
+        x2=1/a
+        
+        if x1<1e-16:
+             return 1
+        return ((x2-x)/(x1))
+
+    
+    i=0   
+    
+    for x in wtDict:
+        betaD=round(float(dfBetaDiv.iloc[i]),3)
+        DalphaMin=round(fnDalphaMin(float(wtDict[x]["n"]),10.0),3)
+        DgammaMax=round(fnDgammaMax(float(wtDict[x]["n"]),10.0, float(k)),3)
+        c=round(con(betaD,DalphaMin,DgammaMax),3)
+        wtDict[x]["con"]=c*100
+        
+        for j in range(wtDict[x]["n"]):
+            wt=dfweight.iloc[i+j]
+            wtDict[x]["Weight"].append(round(wt,4))
+        i=i+wtDict[x]["n"]
+    i=0
+    ii=0
+    for x in wtDict:
+        #st.write(wtDict[x]["con"])
+        tempCon[conMap[ii]]=float(round(wtDict[x]["con"],2))
+        nn=wtDict[x]["n"]        
+        for j in range(nn):
+            dfTemp.at[dfTemp.index[rowMap[i+j]],'weight']= wtDict[x]["Weight"][j]
+        i=i+nn
+        ii=ii+1
+
+    st.session_state.fhi_conc=tempCon
+    st.session_state.fhi_df=dfTemp
+    st.session_state.fhi_wt=True  
+
+if(tempCon[0])!="":   
+    cnt=1
+    container = st.container()
+    col1, col2, col3 = st.columns(3)
+    for x in wtDict:  
+        if cnt==1:
+            with col1:
+                dt=pd.DataFrame.from_dict(wtDict[x]) 
+                t= str(wtDict[x]["title"]) +" [Consensus: "+str(round(wtDict[x]["con"],2))+"%]"
+                fig = px.pie(dt, values='Weight', names='Indicator', title=t,hole=.3)
+                fig.update_layout(showlegend=False)
+                st.plotly_chart(fig, use_container_width=True)
+        if cnt==2:
+            with col2:
+                dt=pd.DataFrame.from_dict(wtDict[x]) 
+                t= str(wtDict[x]["title"]) +" [Consensus: "+str(round(wtDict[x]["con"],2))+"%]"
+                fig = px.pie(dt, values='Weight', names='Indicator', title=t,hole=.3)
+                fig.update_layout(showlegend=False)
+                st.plotly_chart(fig, use_container_width=True)
+        if cnt==3:
+            with col3:
+                dt=pd.DataFrame.from_dict(wtDict[x]) 
+                t= str(wtDict[x]["title"]) +" [Consensus: "+str(round(wtDict[x]["con"],2))+"%]"
+                fig = px.pie(dt, values='Weight', names='Indicator', title=t,hole=.3)
+                fig.update_layout(showlegend=False)
+                st.plotly_chart(fig, use_container_width=True)
+        cnt=cnt+1
+        if cnt==4:
+            cnt=1
+
+    for x in wtDict:
+        weightFinal["indicators"]=weightFinal["indicators"]+wtDict[x]["Indicator"]
+        weightFinal["weights"]=weightFinal["weights"]+wtDict[x]["Weight"]
+    #st.write(weightFinal["indicators"])
+    #st.write(weightFinal["weights"])
+    dfW=pd.DataFrame.from_dict(weightFinal)
+    fig2 = px.bar(dfW, x='indicators', y='weights', color='weights', height=450, title="Weights Summary")
+    container.plotly_chart(fig2, use_container_width=True)
+
+
+    
